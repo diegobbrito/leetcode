73ba63e11535064d2ca684d0de6e620b68a47d8a
Diego Brito <diego.brito@f1rst.com.br>
2024-03-30 19:41:02 -0300

    Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
    Specifically, ans is the concatenation of two nums arrays.
    
    Return the array ans.

--- src/main/java/com/leetcode/arrays/ConcatenationArray.java
+++ src/main/java/com/leetcode/arrays/ConcatenationArray.java
@@ -1,6 +1,8 @@
 package com.leetcode.arrays;
 
 public class ConcatenationArray {
+
+//	https://leetcode.com/problems/concatenation-of-array/description/
 	public int[] getConcatenation(int[] nums) {
 		int[] ans = new int[nums.length * 2];
 		for(int i = 0; i < nums.length; i++){
