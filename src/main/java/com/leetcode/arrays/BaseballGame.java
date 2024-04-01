e8701192b4c12a19fd19f447de095ca12f008a5d
Diego Brito <diego.brito@f1rst.com.br>
2024-03-31 22:11:27 -0300

    Created two different solutions: 1- Using an int array and a normal switch case for the operations 2- After seeing the solutions in the leetcode site, I improved a solution using Stack and use the new switch case with -> operator
    The second solution has a better example in the use of stacks and its methods like push to add an element to the end of the stack, pop to remove and last element of the stack and peek to see the last element of a stack.
    NOTE: Stack is a LIFO: Last In, First Out.

---
+++ src/main/java/com/leetcode/arrays/BaseballGame.java
@@ -0,0 +1,57 @@
+package com.leetcode.arrays;
+
+import java.util.Stack;
+
+public class BaseballGame {
+
+//	https://leetcode.com/problems/baseball-game/
+	public static int calPoints(String[] operations) {
+		int[] points = new int[operations.length];
+		int count = 0;
+		for (String operation : operations) {
+			switch (operation) {
+				case "C":
+					count--;
+					break;
+				case "D":
+					points[count] = 2 * points[count - 1];
+					count++;
+					break;
+				case "+":
+					points[count] = points[count - 1] + points[count - 2];
+					count++;
+					break;
+				default:
+					points[count] = Integer.parseInt(operation);
+					count++;
+			}
+		}
+		int sum = 0;
+		count--;
+		for (int i = count; i >= 0 ; i--) {
+			sum += points[i];
+		}
+		return sum;
+	}
+
+	public int calPointsWithStack(String[] operations) {
+		Stack<Integer> stack = new Stack<>();
+		for(String step : operations) {
+			switch (step) {
+				case "+" -> {
+					int second = stack.pop();
+					int first = stack.peek();
+					stack.push(second);
+					stack.push(second + first);
+				}
+				case "D" -> stack.push(stack.peek() * 2);
+				case "C" -> stack.pop();
+				default -> stack.push(Integer.parseInt(step));
+			}
+		}
+		int sum = 0;
+		while (!stack.isEmpty())
+			sum += stack.pop();
+		return sum;
+	}
+}
