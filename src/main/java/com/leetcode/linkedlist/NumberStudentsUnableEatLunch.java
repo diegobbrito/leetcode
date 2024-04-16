d65a0916b863e3c859d5a8be1db0d3d3905a7612
Diego Brito <diego.brito@f1rst.com.br>
2024-04-15 22:31:08 -0300

    I spent an hour to come with a solution to this problem, first I was using a different queue that moved the element by the number. Then I changed to an ArrayQueue and be able to finish the problem, using a count to stop the while loop. This exercises don't have a video solution in neetcode site, then I saw some of the solutions at leet code site and try to understand a better way to make the while loop finish. I saw the solution comparing the students that have had the sandwich with the size of the students queue. I also changed the Queue to the Linked List.

---
+++ src/main/java/com/leetcode/linkedlist/NumberStudentsUnableEatLunch.java
@@ -0,0 +1,33 @@
+package com.leetcode.linkedlist;
+
+import java.util.*;
+
+public class NumberStudentsUnableEatLunch {
+//	https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/description/
+
+	public static int countStudents(int[] students, int[] sandwiches) {
+		Stack<Integer> sandwichesStack =  new Stack<>();
+		Queue<Integer> studentsQueue = new LinkedList<>();
+
+		int j = sandwiches.length -1;
+		for (int k : students) {
+			studentsQueue.offer(k);
+			sandwichesStack.push(sandwiches[j--]);
+		}
+
+		var studentsThatHadAte = 0;
+		while (!studentsQueue.isEmpty() && studentsThatHadAte < studentsQueue.size()){
+
+			if(studentsQueue.peek().equals(sandwichesStack.peek())){
+				sandwichesStack.pop();
+				studentsThatHadAte = 0;
+			} else {
+				studentsQueue.offer(studentsQueue.peek());
+				studentsThatHadAte++;
+			}
+			studentsQueue.poll();
+		}
+		return studentsQueue.size();
+	}
+
+}
