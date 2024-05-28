d694ca37ca792f4f5b8f245796c32ff29378f8d6
Diego Brito <diego.brito@f1rst.com.br>
2024-05-27 21:23:15 -0300

    Today I learned about BinarySearch, then I implemented this problem.

---
+++ src/main/java/com/leetcode/searching/BinarySearch.java
@@ -0,0 +1,24 @@
+package com.leetcode.searching;
+
+public class BinarySearch {
+//	https://leetcode.com/problems/binary-search/description/
+
+// Time complexity O(log(n))
+//	Memory complecity O(1)
+	public int search(int[] nums, int target) {
+		int left = 0;
+		int right = nums.length - 1;
+
+		while (left <= right){
+			//Here its calculated the distance between left and right, then adding left to get the middle
+			// This avoid overflow if the numbers are close to the maximum integer in the (left + right) /2
+			int middle = left + (right - left) / 2;
+			if (target > nums[middle])
+			left = middle + 1;
+			else if(target < nums[middle])
+			right = middle - 1;
+			else return middle;
+		}
+		return -1;
+	}
+}
