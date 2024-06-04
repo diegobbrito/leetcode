dc2db97c0f3848db8ab04fb0984c6fafffb579b4
Diego Brito <diego.brito@f1rst.com.br>
2024-06-03 21:10:40 -0300

    I started to learn about Trees, this was my first creation about searching a binary tree. It's the same principle of the binary search, going left or right depending on the comparison between the target value and the current treenode

---
+++ src/main/java/com/leetcode/trees/SearchBinarySearchTree.java
@@ -0,0 +1,27 @@
+package com.leetcode.trees;
+
+public class SearchBinarySearchTree {
+	public TreeNode searchBST(TreeNode root, int val) {
+		if(root == null) return null;
+
+		if(val > root.val)
+			return searchBST(root.right, val);
+		else if(val < root.val)
+			return searchBST(root.left, val);
+		else
+			return root;
+	}
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode() {}
+	TreeNode(int val) { this.val = val; }
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
