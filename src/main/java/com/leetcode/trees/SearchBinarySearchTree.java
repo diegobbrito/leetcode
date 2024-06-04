38c1a68a2910b173814eaca103b7e39383f153d1
Diego Brito <diego.brito@f1rst.com.br>
2024-06-04 20:00:59 -0300

    Update the tree node in a new class

--- src/main/java/com/leetcode/trees/SearchBinarySearchTree.java
+++ src/main/java/com/leetcode/trees/SearchBinarySearchTree.java
@@ -12,16 +12,3 @@ public class SearchBinarySearchTree {
 			return root;
 	}
 }
-
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-	TreeNode() {}
-	TreeNode(int val) { this.val = val; }
-	TreeNode(int val, TreeNode left, TreeNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
-	}
-}
