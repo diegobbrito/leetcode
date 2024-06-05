51c7f34167543a8cc3a40ca088829dfe0ba51d9e
Diego Brito <diego.brito@f1rst.com.br>
2024-06-04 21:27:10 -0300

    This was my second solution after reading others solutions, understand better the use of the binary search

--- src/main/java/com/leetcode/searching/FirstBadVersion.java
+++ src/main/java/com/leetcode/searching/FirstBadVersion.java
@@ -2,26 +2,20 @@ package com.leetcode.searching;
 
 public class FirstBadVersion {
 //	https://leetcode.com/problems/first-bad-version/
-	public int firstBadVersion(int n) {
-		if(isBadVersion(1)) return 1;
+public int firstBadVersion(int n) {
+	int left = 1;
+	int right = n;
 
-		int left = 2;
-		int right = n;
+	while(left <  right){
 		int middle = left + (right - left) / 2;
-		boolean badVersion = isBadVersion(middle);
-		boolean goodVersion = isBadVersion(middle - 1);
-		while(badVersion && goodVersion || !badVersion && !goodVersion){
-			if(badVersion){
-				right = middle - 1;
-			}else{
-				left = middle + 1;
-			}
-			middle = left + (right - left) / 2;
-			badVersion = isBadVersion(middle);
-			goodVersion = isBadVersion(middle - 1);
+		if (isBadVersion(middle)) {
+			right = middle;
+		} else {
+			left = middle + 1;
 		}
-		return middle;
 	}
+	return right;
+}
 
 //	Method provide by the exercise;
 	private boolean isBadVersion(int i) {
