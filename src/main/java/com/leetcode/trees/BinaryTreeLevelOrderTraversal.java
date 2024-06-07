d2d1ec9c5271955a5836f23d4a0e67c007c298c5
Diego Brito <diego.brito@f1rst.com.br>
2024-06-07 09:59:45 -0300

    Fix the for loop using the size of queue before starting the loop. Update methods names

--- src/main/java/com/leetcode/trees/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/com/leetcode/trees/BinaryTreeLevelOrderTraversal.java
@@ -9,21 +9,22 @@ public class BinaryTreeLevelOrderTraversal {
 
 	public List<List<Integer>> levelOrder(TreeNode root) {
 		List<List<Integer>> response = new ArrayList<>();
-
 		Queue<TreeNode> queue = new LinkedList<>();
-		if (root == null) return response;
-		queue.add(root);
+
+		if (root != null)
+			queue.add(root);
 
 		while (!queue.isEmpty()) {
 			List<Integer> list = new ArrayList<>();
-			for (int i = 0; i < queue.size(); i++) {
+			int queueSize = queue.size();
+			for (int i = 0; i < queueSize; i++) {
 				TreeNode curr = queue.poll();
-				list.add(curr.val);
-				if (curr.left != null) {
-					queue.add(curr.left);
-				}
-				if (curr.right != null) {
-					queue.add(curr.right);
+				if(curr != null){
+					if (curr.left != null)
+						queue.offer(curr.left);
+					if (curr.right != null)
+						queue.offer(curr.right);
+					list.add(curr.val);
 				}
 			}
 			response.add(list);
