6ce114238881b54842a1e3145371f2b2253f08da
Diego Brito <diego.brito@f1rst.com.br>
2024-06-07 21:38:38 -0300

    Learned about DFS algorithm

---
+++ src/main/java/com/leetcode/trees/DFSTraversal.java
@@ -0,0 +1,36 @@
+package com.leetcode.trees;
+
+import com.sun.source.tree.Tree;
+
+import java.util.List;
+
+public class DFSTraversal {
+
+//	DFS PreOrder
+//	Node -> Left -> Right
+	public void preorder(TreeNode root, List<Integer> nums){
+		if (root != null){
+			nums.add(root.val);
+			preorder(root.left, nums);
+			preorder(root.right, nums);
+		}
+	}
+	//	DFS InOrder
+//	 Left -> Node -> Right
+	public void inorder(TreeNode root, List<Integer> nums){
+		if (root != null){
+			inorder(root.left, nums);
+			nums.add(root.val);
+			inorder(root.right, nums);
+		}
+	}
+	//	DFS PosOrder
+//	Left -> Right -> Node
+	public void posorder(TreeNode root, List<Integer> nums){
+		if (root != null){
+			posorder(root.left, nums);
+			posorder(root.right, nums);
+			nums.add(root.val);
+		}
+	}
+}
