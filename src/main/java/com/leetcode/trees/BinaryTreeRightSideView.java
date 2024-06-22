ce8d2016966290a17e70ca9149f385845e0a50a8
Diego Brito <dibbrito@gmail.com>
2024-06-21 21:52:11 -0300

    Add BFS Solution for leetcode problem

--- src/main/java/com/leetcode/trees/BinaryTreeRightSideView.java
+++ src/main/java/com/leetcode/trees/BinaryTreeRightSideView.java
@@ -6,37 +6,30 @@ import java.util.List;
 import java.util.Queue;
 
 public class BinaryTreeRightSideView {
+//	https://leetcode.com/problems/binary-tree-right-side-view/
+//	BFS Solution
 	public List<Integer> rightSideView(TreeNode root) {
 		List<Integer> response = new ArrayList<>();
-		if(root == null) return response;
-
-		while (root != null && root.right != null){
-			response.add(root.val);
-			root = root.right;
-		}
-		return response;
-	}
-
-	public List<Integer> levelOrder(TreeNode root) {
-		List<Integer> response = new ArrayList<>();
 		Queue<TreeNode> queue = new LinkedList<>();
-
-		if (root != null)
-			queue.add(root);
+		queue.add(root);
 
 		while (!queue.isEmpty()) {
+			TreeNode rightSide = null;
 			int queueSize = queue.size();
 			for (int i = 0; i < queueSize; i++) {
 				TreeNode curr = queue.poll();
 				if(curr != null){
+					rightSide = curr;
 					if (curr.left != null)
 						queue.offer(curr.left);
 					if (curr.right != null)
 						queue.offer(curr.right);
-					response.add(curr.val);
 				}
 			}
+			if (rightSide != null)
+				response.add(rightSide.val);
 		}
+
 		return response;
 	}
 }
