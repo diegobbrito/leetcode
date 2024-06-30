e757c79300928d0663c401282bbe8e6a0e0c43d9
Diego Brito <dibbrito@gmail.com>
2024-06-30 14:16:59 -0300

    Create another solution for the Group Anagrams problem, using map and sorted keys

--- src/main/java/com/leetcode/arrays/GroupAnagrams.java
+++ src/main/java/com/leetcode/arrays/GroupAnagrams.java
@@ -1,54 +1,30 @@
 package com.leetcode.arrays;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 
 public class GroupAnagrams {
+    //    https://leetcode.com/problems/group-anagrams/
     public static void main(String[] args) {
         var response = groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
         System.out.println(response);
     }
 
     public static List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> response = new ArrayList<>();
-        boolean[] cache = new boolean[strs.length];
-        for (int i = 0; i < strs.length - 1; i++) {
-            List<String> current = new ArrayList<>();
-            for (int j = i + 1; j < strs.length; j++) {
-                if (!cache[j]) {
-                    if (isAnagram(strs[i], strs[j])) {
-                        cache[j] = true;
-                        cache[i] = true;
-                        current.add(strs[i]);
-                        current.add(strs[j]);
-                    }
-                }
+        // Create a map to add as key the sorted characters
+        // and as value a List, then adding the values in this list
+        // finally returning the list of lists with the group of anagrams
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            var chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new ArrayList<>());
             }
-            if (!current.isEmpty())
-                response.add(List.copyOf(current));
+            map.get(sorted).add(str);
         }
-        if (!cache[strs.length - 1]) {
-            response.add(List.of(strs[strs.length - 1]));
-        }
-
-        return response;
+        return new ArrayList<>(map.values());
     }
 
-    private static boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        if (s.equals(t)) return true;
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
-        }
-
-        for (Integer i : map.values()) {
-            if (i != 0) return false;
-        }
-
-        return true;
-    }
 }
