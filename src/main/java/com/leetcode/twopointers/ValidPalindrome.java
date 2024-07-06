1f58f047d3c9312ccbf6833aea577dbd0c26c175
Diego Brito <dibbrito@gmail.com>
2024-07-05 21:39:12 -0300

    My solution for this problem, using only a for loop

---
+++ src/main/java/com/leetcode/twopointers/ValidPalindrome.java
@@ -0,0 +1,18 @@
+package com.leetcode.twopointers;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("0P"));
+    }
+
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int len = s.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(i) != s.charAt(len - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
