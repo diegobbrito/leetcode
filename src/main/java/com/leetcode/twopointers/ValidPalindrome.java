adb326782dd6355b31ec01af0e83b377b5000944
Diego Brito <dibbrito@gmail.com>
2024-07-06 20:18:40 -0300

    Update the solution using my own method to check if the character is not alphanumeric

--- src/main/java/com/leetcode/twopointers/ValidPalindrome.java
+++ src/main/java/com/leetcode/twopointers/ValidPalindrome.java
@@ -2,43 +2,45 @@ package com.leetcode.twopointers;
 
 public class ValidPalindrome {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("0P"));
+        System.out.println(isPalindromeTwoPointers("bana  @anab"));
     }
 
     public static boolean isPalindrome(String s) {
         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
         int len = s.length();
         for (int i = 0; i < len / 2; i++) {
-            if (s.charAt(i) != s.charAt(len - i - 1)) {
+            if (s.charAt(i) != s.charAt(len - i - 1))
                 return false;
-            }
         }
         return true;
     }
 
-    public boolean isPalindromeTwoPointers(String s) {
+    public static boolean isPalindromeTwoPointers(String s) {
 
         int i = 0;
         int j = s.length() - 1;
         while (i < j) {
-
             char start = s.charAt(i);
             char end = s.charAt(j);
-
-            if (!Character.isLetterOrDigit(start)) {
+            if (isNotAlphanumeric(start)) {
                 i++;
-                continue;
+                continue; // Used instead another while loop
             }
-            if (!Character.isLetterOrDigit(end)) {
+            if (isNotAlphanumeric(end)) {
                 j--;
                 continue;
             }
-            if (Character.toLowerCase(start) != Character.toLowerCase(end)) {
+            if (Character.toLowerCase(start) != Character.toLowerCase(end))
                 return false;
-            }
             i++;
             j--;
         }
         return true;
+    }
+
+    private static boolean isNotAlphanumeric(char character) {
+        return (character < 'a' || character > 'z') &&
+                (character < 'A' || character > 'Z') &&
+                (character < '0' || character > '9');
     }
 }
