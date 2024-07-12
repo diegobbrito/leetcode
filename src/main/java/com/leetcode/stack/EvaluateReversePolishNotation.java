489301b09e921319141838e23aff7e1cd23a7b10
Diego Brito <dibbrito@gmail.com>
2024-07-11 21:21:48 -0300

    After understanding the polish notation, the solve for this problem was a simple stack with a switch method for each string in the array

---
+++ src/main/java/com/leetcode/stack/EvaluateReversePolishNotation.java
@@ -0,0 +1,31 @@
+package com.leetcode.stack;
+
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation {
+    //    https://leetcode.com/problems/evaluate-reverse-polish-notation/
+//    Time Complexity O(n)
+//    Memory complexity O(n)
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            switch (token) {
+                case "*" -> stack.push(stack.pop() * stack.pop());
+                case "-" -> {
+                    var temp = stack.pop();
+                    stack.push(stack.pop() - temp);
+                }
+                case "+" -> {
+                    var temp = stack.pop();
+                    stack.push(stack.pop() + temp);
+                }
+                case "/" -> {
+                    var temp = stack.pop();
+                    stack.push(stack.pop() / temp);
+                }
+                default -> stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+}
