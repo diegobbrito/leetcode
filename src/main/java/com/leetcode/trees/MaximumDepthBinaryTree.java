c863c99165e4a9cc3933780e15d69e00e510cab6
Diego Brito <dibbrito@gmail.com>
2024-07-16 21:02:27 -0300

    Add one more solution for Maximum Depth Binary Tree problem with BFS

--- src/main/java/com/leetcode/trees/MaximumDepthBinaryTree.java
+++ src/main/java/com/leetcode/trees/MaximumDepthBinaryTree.java
@@ -1,5 +1,8 @@
 package com.leetcode.trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MaximumDepthBinaryTree {
 //    https://leetcode.com/problems/maximum-depth-of-binary-tree/
 
@@ -10,6 +13,24 @@ public class MaximumDepthBinaryTree {
         if (root == null)
             return 0;
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    // BFS
+    public int maxDepthBFS(TreeNode root) {
+        if (root == null) return 0;
+        int depth = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                var node = queue.poll();
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            depth++;
+        }
+        return depth;
     }
 
 
