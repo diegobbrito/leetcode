48a0bb6220a6efbc410d699c7462656d1ea52a11
Diego Brito <diego.brito@f1rst.com.br>
2024-08-01 19:35:00 -0300

    Add solution for FindIndexFirstOccurrenceString

---
+++ src/main/java/com/leetcode/twopointers/FindIndexFirstOccurrenceString.java
@@ -0,0 +1,39 @@
+package com.leetcode.twopointers;
+
+public class FindIndexFirstOccurrenceString {
+//	https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+public static void main(String[] args) {
+	System.out.println(strStr("mississippi","pi"));
+}
+	public static int strStr(String haystack, String needle) {
+
+		int needleSize = needle.length();
+		int hayStackSize = haystack.length();
+		if(hayStackSize < needleSize) return -1;
+
+		int needleIndex = 0, haystackIndex = 0;
+
+		while(haystackIndex < hayStackSize){
+			if(haystack.charAt(haystackIndex) == needle.charAt(needleIndex)){
+				while(haystackIndex < hayStackSize
+						&& needleIndex < needleSize
+						&& haystack.charAt(haystackIndex) == needle.charAt(needleIndex)){
+					haystackIndex++;
+					needleIndex++;
+				}
+				if(needleIndex == needleSize)
+					return haystackIndex - needleSize;
+				else{
+					haystackIndex -= needleIndex;
+					needleIndex = 0;
+				}
+			}
+			haystackIndex++;
+		}
+		return -1;
+	}
+
+	public int strStr2(String haystack, String needle) {
+		return haystack.indexOf(needle);
+	}
+}
