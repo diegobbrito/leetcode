f0b8f2315f845c269c71e355a937a52e65a5526a
Diego Brito <dibbrito@gmail.com>
2024-08-17 19:57:32 -0300

    Add little improvement and comment BubbleSort code

--- src/main/java/com/leetcode/sorting/BubbleSort.java
+++ src/main/java/com/leetcode/sorting/BubbleSort.java
@@ -4,8 +4,8 @@ public class BubbleSort {
 
 //	Complexity time = O(nˆ2)
 	public void bubbleSort(int[] array){
-		for (int i = 0; i < array.length - 1; i++) {
-			for (int j = 0; j < array.length - i - 1; j++) {
+		for (int i = 0, length = array.length; i < length - 1; i++) {
+			for (int j = 0; j < length - i - 1; j++) { // the length - i - 1 is because the last element will be sorted
 				if(array[j] > array[j + 1]){
 					var temp = array[j];
 					array[j] = array[j+1];
