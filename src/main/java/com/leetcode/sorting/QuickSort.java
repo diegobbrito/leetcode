bda43e138d6617ead2a31334fc5d4137b51219ae
Diego Brito <dibbrito@gmail.com>
2024-08-17 19:58:15 -0300

    Add comments to QuickSort code

--- src/main/java/com/leetcode/sorting/QuickSort.java
+++ src/main/java/com/leetcode/sorting/QuickSort.java
@@ -15,24 +15,32 @@ public class QuickSort {
 	}
 
 	private static void quickSort(int[] array, int start, int end) {
-
+		//Call recursively until the end is less or equal to the start
 		if(end <= start) return;
 
+		// Get the pivot index, will at the right index
 		int pivot = partition(array, start, end);
 
+		//Sort the array to the start until pivot -1
 		quickSort(array, start, pivot - 1);
+		//Sort the array to the pivot + 1 until the end
 		quickSort(array, pivot + 1, end);
 	}
 
 	private static int partition(int[] array, int start, int end){
+		//Select a pivot element, this case, the last one
 		int pivot = array[end];
+		// Get the i and j with the start index
 		int i = start;
+		//Iterate between start and end element, swapping all the element that and less than the pivot value
 		for (int j = start; j < end; j++) {
 			if(array[j] < pivot){
 				swap(array, i, j);
 				i++;
 			}
 		}
+		// Swap the element at i index with the element at the end (the pivot index)
+		// All element at the left will be smaller and all element to the right will be bigger than the pivot
 		swap(array, i, end);
 		return i;
 	}
