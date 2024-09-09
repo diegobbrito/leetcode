bb3080019c0f83e7d245a1e4eac0ed36d6a09b83
Diego Brito <dibbrito@gmail.com>
2024-09-09 20:43:28 -0300

    Add second solution

--- src/main/java/com/leetcode/arrays/ValidAnagram.java
+++ src/main/java/com/leetcode/arrays/ValidAnagram.java
@@ -1,8 +1,12 @@
 package com.leetcode.arrays;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ValidAnagram {
+    //    https://leetcode.com/problems/valid-anagram/
+//    Time Complexity O(n)
+//    Space Complexity O(n)
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
         HashMap<Character, Integer> map = new HashMap<>();
@@ -13,6 +17,25 @@ public class ValidAnagram {
 
         for (Integer i: map.values()) {
             if(i != 0) return false;
+        }
+
+        return true;
+    }
+
+    //    Time Complexity O(n)
+    //    Space Complexity O(n)
+    public boolean isAnagram2(String s, String t) {
+        if (s.length() != t.length()) return false;
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) return false;
+            map.put(c, map.get(c) - 1);
+        }
+        for (char c : s.toCharArray()) {
+            if (map.get(c) != 0) return false;
         }
 
         return true;
