68d4622b086eece1744e1e177034f19ecdc67966
Diego Brito <dibbrito@gmail.com>
2024-09-17 20:19:10 -0300

    Add comments to HasDuplicate

--- src/main/java/com/leetcode/arrays/HasDuplicate.java
+++ src/main/java/com/leetcode/arrays/HasDuplicate.java
@@ -4,7 +4,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class HasDuplicate {
+//	https://leetcode.com/problems/contains-duplicate/
 
+	//	Time Complexity O(n)
+//	Memory Complexity O(n)
 	public boolean hasDuplicate(int[] nums) {
 		Set<Integer> hashSet = new HashSet<>();
 		for (int num : nums) {
