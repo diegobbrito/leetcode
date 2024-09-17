1cac9416e79c681edd94f8e5f7c9edce5b06da22
Diego Brito <dibbrito@gmail.com>
2024-09-16 21:09:31 -0300

    Add insert and remove methods for TreeNode

--- src/main/java/com/leetcode/trees/TreeNode.java
+++ src/main/java/com/leetcode/trees/TreeNode.java
@@ -11,4 +11,39 @@ public class TreeNode {
 		this.left = left;
 		this.right = right;
 	}
+
+	private TreeNode insert(TreeNode root, int val) {
+		if (root == null)
+			return new TreeNode(val);
+		if (val > root.val)
+			root.right = insert(root.right, val);
+		else if (val < root.val)
+			root.left = insert(root.left, val);
+		return root;
+	}
+
+	private TreeNode minValueNode(TreeNode root) {
+		var curr = root;
+		while (curr != null && curr.left != null)
+			curr = curr.left;
+		return curr;
+	}
+
+	private TreeNode remove(TreeNode root, int val) {
+		if (root == null) return null;
+		if (val > root.val)
+			root.right = remove(root.right, val);
+		else if (val < root.val)
+			root.left = remove(root.left, val);
+		else {
+			if (root.left == null) return root.right;
+			else if (root.right == null) return root.left;
+			else {
+				var minNode = minValueNode(root.right);
+				root.val = minNode.val;
+				root.right = remove(root.right, root.val);
+			}
+		}
+		return root;
+	}
 }
