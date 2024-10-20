9b56b59a98c21e9795e384db20f6f5350114522e
Diego Brito <dibbrito@gmail.com>
2024-10-20 17:08:32 -0300

    Add one more solution for Unique Paths

--- src/main/java/com/leetcode/dyanmicprogramming/UniquePaths.java
+++ src/main/java/com/leetcode/dyanmicprogramming/UniquePaths.java
@@ -42,4 +42,19 @@ public class UniquePaths {
             return 1;
         return (bruteForce(r + 1, c, rows, cols) + (bruteForce(r, c + 1, rows, cols)));
     }
+
+    // Dynamic programming: TC = O(m*n), SC = O(m*n)
+    public int uniquePathsDP(int m, int n) {
+        int[][] dp = new int[m][n];
+        //    Fill last row and last line with 1
+        for (int i = 0; i < n; i++)
+            dp[m - 1][i] = 1;
+        for (int i = 0; i < m; i++)
+            dp[i][n - 1] = 1;
+        //    Fill the array with the sum of right and bottom for each position
+        for (int i = m - 2; i >= 0; i--)
+            for (int j = n - 2; j >= 0; j--)
+                dp[i][j] = dp[i][j + 1] + dp[i + 1][j];
+        return dp[0][0];    // Return first position
+    }
 }
