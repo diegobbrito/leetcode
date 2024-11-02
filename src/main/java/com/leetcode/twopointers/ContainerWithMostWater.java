41055880c146a34b69b891475bc7570d2700524c
Diego Brito <dibbrito@gmail.com>
2024-11-01 23:46:10 -0300

    update solutions

--- src/main/java/com/leetcode/twopointers/ContainerWithMostWater.java
+++ src/main/java/com/leetcode/twopointers/ContainerWithMostWater.java
@@ -1,23 +1,6 @@
 package com.leetcode.twopointers;
 
 public class ContainerWithMostWater {
-    public static int maxAreaOptimize(int[] height) {
-        // Initialize two pointers: left starting from the beginning and right from the end.
-        int left = 0, right = height.length - 1, max = 0;
-        // Continue looping while the left pointer is less than the right pointer.
-        while (left < right) {
-            // Get the height of the shorter line between the two pointers.
-            int h = Math.min(height[left], height[right]);
-            // Calculate the current water area and update the max area if the new area is larger.
-            max = Math.max(max, h * (right - left));
-            // Move the left pointer inward if the current left height is less than or equal to h.
-            while (left < right && h >= height[left]) left++;
-            // Move the right pointer inward if the current right height is less than or equal to h.
-            while (left < right && h >= height[right]) right--;
-        }
-        // Return the maximum area found.
-        return max;
-    }
 
     //    https://leetcode.com/problems/container-with-most-water/
 //    Time Complexity O(n)
@@ -41,6 +24,26 @@ public class ContainerWithMostWater {
                 left++;  // If the left line is shorter, move it to the right.
             else
                 right--;  // Otherwise, move the right pointer to the left.
+        }
+        // Return the maximum area found.
+        return max;
+    }
+
+    //    Time Complexity O(n)
+    //    Memory Complexity O(1)
+    public int maxAreaOptimize(int[] height) {
+        // Initialize two pointers: left starting from the beginning and right from the end.
+        int left = 0, right = height.length - 1, max = 0;
+        // Continue looping while the left pointer is less than the right pointer.
+        while (left < right) {
+            // Get the height of the shorter line between the two pointers.
+            int h = Math.min(height[left], height[right]);
+            // Calculate the current water area and update the max area if the new area is larger.
+            max = Math.max(max, h * (right - left));
+            // Move the left pointer inward if the current left height is less than or equal to h.
+            while (left < right && h >= height[left]) left++;
+            // Move the right pointer inward if the current right height is less than or equal to h.
+            while (left < right && h >= height[right]) right--;
         }
         // Return the maximum area found.
         return max;
