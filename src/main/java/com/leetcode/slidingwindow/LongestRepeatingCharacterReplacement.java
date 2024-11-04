823d90a23634c6f076710e344b66d6ab4eac8e62
Diego Brito <dibbrito@gmail.com>
2024-11-04 19:59:13 -0300

    add solution for LongestRepeatingCharacterReplacement

---
+++ src/main/java/com/leetcode/slidingwindow/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,25 @@
+package com.leetcode.slidingwindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestRepeatingCharacterReplacement {
+    //    https://leetcode.com/problems/longest-repeating-character-replacement/
+    public int characterReplacement(String s, int k) {
+        int left = 0;
+        int result = 0;
+        int maxFrequency = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int right = 0; right < s.length(); right++) {
+            map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
+            maxFrequency = Math.max(maxFrequency, map.get(s.charAt(right)));
+
+            while ((right - left + 1) - maxFrequency > k) {
+                map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
+                left++;
+            }
+            result = Math.max(result, right - left + 1);
+        }
+        return result;
+    }
+}
