3a28146bfe80d9979ddcf7ba658ebea42e63ecc4
Diego Brito <54558396+Diegobbrito@users.noreply.github.com>
2024-11-21 00:47:30 +0000

    Add another solution with List

--- src/main/java/com/leetcode/arrays/IntersectionTwoArrays.java
+++ src/main/java/com/leetcode/arrays/IntersectionTwoArrays.java
@@ -1,5 +1,7 @@
 package com.leetcode.arrays;
 
+import java.util.Set;
+
 public class IntersectionTwoArrays {
     //https://leetcode.com/problems/intersection-of-two-arrays/
 
@@ -11,7 +13,6 @@ public class IntersectionTwoArrays {
             set1.add(i);
         }
         Set<Integer> set2 = new HashSet<>();
-        int count = 0;
         for(int i : nums2){
             set2.add(i);
         }
@@ -24,6 +25,29 @@ public class IntersectionTwoArrays {
         int []result = new int[set.size()];
         int i = 0;
         for(int value : set){
+            result[i++] = value;
+        }
+        return result;
+    }
+
+    // Time Complexity O(n + m)
+    // Space Complexity O(n)
+    public int[] intersection2(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        for(int i : nums1){
+            set.add(i);
+        }
+        List<Integer> list = new ArrayList<>();
+        for(int i : nums2){
+            if (set.contains(i)){
+                list.add(i);
+                set.remove(i);
+            }
+        }
+
+        int []result = new int[list.size()];
+        int i = 0;
+        for(int value : list){
             result[i++] = value;
         }
         return result;
