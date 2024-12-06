4dc75f77f660609a4f5161d907bfdb02f8359d14
Diego Brito <dibbrito@gmail.com>
2024-12-06 19:06:23 -0300

    Update solution removing list

--- src/main/java/com/leetcode/trees/MinimumDistanceBetweenBSTNodes.java
+++ src/main/java/com/leetcode/trees/MinimumDistanceBetweenBSTNodes.java
@@ -1,30 +1,25 @@
 package com.leetcode.trees;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MinimumDistanceBetweenBSTNodes {
     //    https://leetcode.com/problems/minimum-distance-between-bst-nodes/
 // Time Complexity O(n)
 // Space Complexity O(n)
+    int min;
+    int last;
     public int minDiffInBST(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        dfs(root, list);
-        int min = Integer.MAX_VALUE;
-        int last = Integer.MAX_VALUE;
-        for (int i : list) {
-            int diff = Math.abs(i - last);
-            min = Math.min(min, diff);
-            last = i;
-        }
+        min = Integer.MAX_VALUE;
+        last = Integer.MAX_VALUE;
+        dfs(root);
         return min;
     }
 
-    private void dfs(TreeNode node, List<Integer> list) {
+    private void dfs(TreeNode node) {
         if (node != null) {
-            dfs(node.left, list);
-            list.add(node.val);
-            dfs(node.right, list);
+            dfs(node.left);
+            int diff = Math.abs(node.val - last);
+            min = Math.min(min, diff);
+            last = node.val;
+            dfs(node.right);
         }
     }
 }
