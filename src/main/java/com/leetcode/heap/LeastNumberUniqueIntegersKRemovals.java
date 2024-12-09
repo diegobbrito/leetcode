e32306dd76aaa14110a4b111ff5789abb43c8363
Diego Brito <dibbrito@gmail.com>
2024-12-09 15:26:56 -0300

    update solution for LeastNumberUniqueIntegersKRemovals

--- src/main/java/com/leetcode/heap/LeastNumberUniqueIntegersKRemovals.java
+++ src/main/java/com/leetcode/heap/LeastNumberUniqueIntegersKRemovals.java
@@ -6,45 +6,32 @@ import java.util.PriorityQueue;
 
 public class LeastNumberUniqueIntegersKRemovals {
 //    https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/
+//Time Complexity
+//Building the frequency map: O(N).
+//Adding frequencies to the heap: O(MlogM), where M≤N is the number of unique integers.
+//Removing k elements: O(klogM).
+//Overall Time Complexity: O(N+MlogM+klogM).
+//In the worst case, M=N, so this simplifies to O(NlogN).
 
+    //    Space Complexity
+//Frequency map: O(M), where M is the number of unique integers.
+//PriorityQueue: O(M).
+//Overall Space Complexity: O(M).
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
 
-        PriorityQueue<Pair<Integer, Integer>> pq = new PriorityQueue<>();
-
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i : arr) {
+        for (int i : arr)
             map.put(i, map.getOrDefault(i, 0) + 1);
-        }
-        for (var i : map.entrySet()) {
-            pq.add(new Pair<>(i.getKey(), i.getValue()));
-        }
+
+        pq.addAll(map.values());
+
         for (int i = 0; i < k; i++) {
-            var pair = pq.poll();
-            if (pair.value > 1) {
-                pq.add(new Pair<>(pair.key, pair.value - 1));
-            }
+            var value = pq.poll();
+            if (value != null && value > 1)
+                pq.add(value - 1);
         }
         return pq.size();
-    }
-
-    static class Pair<K, V> implements Comparable<Pair<K, V>> {
-        K key;
-        V value;
-
-        // Constructor to initialize the pair with key and value
-        public Pair(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-
-        @Override
-        public int compareTo(Pair<K, V> o) {
-            if ((int) this.value > (int) o.value)
-                return 1;
-            else if (this.value == o.value)
-                return 0;
-            return -1;
-        }
     }
 }
