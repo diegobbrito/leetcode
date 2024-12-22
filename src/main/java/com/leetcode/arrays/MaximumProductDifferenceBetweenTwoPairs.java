8ca469f22de5c64531e01b8fcd117297197a4ff5
Diego Brito <dibbrito@gmail.com>
2024-12-22 11:16:49 -0300

    Add optimize solution for MaximumProductDifferenceBetweenTwoPairs

--- src/main/java/com/leetcode/arrays/MaximumProductDifferenceBetweenTwoPairs.java
+++ src/main/java/com/leetcode/arrays/MaximumProductDifferenceBetweenTwoPairs.java
@@ -8,6 +8,7 @@ public class MaximumProductDifferenceBetweenTwoPairs {
     //    https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
 //    Time Complexity O(nlogn)
 //    Space Complexity O(n)
+//    Priority Queue
     public int maxProductDifference(int[] nums) {
         PriorityQueue<Integer> min = new PriorityQueue<>();
         PriorityQueue<Integer> max = new PriorityQueue<>((a, b) -> b - a);
@@ -25,8 +26,37 @@ public class MaximumProductDifferenceBetweenTwoPairs {
         return (c * d) - (a * b);
     }
 
+    //    Time Complexity O(nlogn)
+//    Space Complexity O(n)
+//    Sorting
     public int maxProductDifferenceSorting(int[] nums) {
         Arrays.sort(nums);
         return (nums[nums.length - 1] * nums[nums.length - 2]) - (nums[0] * nums[1]);
+    }
+
+    //    Time Complexity O(n)
+//    Space Complexity O(1)
+//    Two Maximums and Two Minimums
+    public int maxProductDifferenceOptimize(int[] nums) {
+        int max1 = 0, max2 = 0;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num > max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max2 = num;
+            }
+
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+        }
+
+        return (max1 * max2) - (min1 * min2);
     }
 }
