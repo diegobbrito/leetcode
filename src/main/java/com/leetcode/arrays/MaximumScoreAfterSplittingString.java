659b15960c02f6514dc520029b6b860b64094c1e
Diego Brito <dibbrito@gmail.com>
2024-12-22 19:16:24 -0300

    Add more two solutions for MaximumScoreAfterSplittingString

--- src/main/java/com/leetcode/arrays/MaximumScoreAfterSplittingString.java
+++ src/main/java/com/leetcode/arrays/MaximumScoreAfterSplittingString.java
@@ -3,8 +3,7 @@ package com.leetcode.arrays;
 public class MaximumScoreAfterSplittingString {
 
 //    https://leetcode.com/problems/maximum-score-after-splitting-a-string/
-
-    //    Time Complexity - O(n)
+//    Time Complexity - O(n)
 //    Space Complexity - O(n)
     public int maxScore(String s) {
         int[] preFix = new int[s.length()];
@@ -21,6 +20,43 @@ public class MaximumScoreAfterSplittingString {
                 count++;
             result = Math.max(result, count + preFix[left]);
         }
+        return result;
+    }
+
+//    Iteration (One Pass)
+//    Time Complexity - O(n)
+//    Space Complexity - O(1)
+
+    public int maxScoreOptimize(String s) {
+        int zeros = 0, ones = 0, result = -1;
+        if (s.charAt(0) == '0') zeros++;
+        else ones++;
+
+        for (int i = 1; i < s.length(); i++) {
+            result = Math.max(result, zeros - ones);
+            if (s.charAt(i) == '1') ones++;
+            else zeros++;
+        }
+        return result + ones;
+    }
+
+//    Iteration (Two Pass)
+//    Time Complexity - O(n)
+//    Space Complexity - O(1)
+
+    public int maxScoreIteration(String s) {
+        int zeros = 0, ones = 0, result = -1;
+
+        for (char c : s.toCharArray()) {
+            if (c == '1') ones++;
+        }
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '0') zeros++;
+            else ones--;
+            result = Math.max(result, zeros + ones);
+        }
+
         return result;
     }
 }
