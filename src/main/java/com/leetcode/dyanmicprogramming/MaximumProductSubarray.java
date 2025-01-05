6e20c43e1faa9a2981aae2cc2b7b2f1bc72748c2
Diego Brito <dibbrito@gmail.com>
2025-01-04 22:07:33 -0300

    Add more comments to the solution

--- src/main/java/com/leetcode/dyanmicprogramming/MaximumProductSubarray.java
+++ src/main/java/com/leetcode/dyanmicprogramming/MaximumProductSubarray.java
@@ -3,6 +3,8 @@ package com.leetcode.dyanmicprogramming;
 public class MaximumProductSubarray {
 
     //    https://leetcode.com/problems/maximum-product-subarray/
+
+    //Kadane's Algorithm
     //    Time complexity: O(n)
     //    Space complexity: O(1)
     public int maxProduct(int[] nums) {
@@ -10,10 +12,11 @@ public class MaximumProductSubarray {
         int min = nums[0];
         int result = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            int temp = max;//store the max value
-            int currentMax = Math.max(max * nums[i], min * nums[i]);
+            int tempMax = max * nums[i];//store the max value
+            int tempMin = min * nums[i];//store the min value
+            int currentMax = Math.max(tempMax, tempMin);
             max = Math.max(currentMax, nums[i]);//max is the max of max*nums[i], min*nums[i] and nums[i]
-            int currentMin = Math.min(temp * nums[i], min * nums[i]);
+            int currentMin = Math.min(tempMax, tempMin);
             min = Math.min(currentMin, nums[i]); //min is the min of max*nums[i], min*nums[i] and nums[i]
             result = Math.max(result, max); //result is the max of result and max
         }
