dabbbf1848a2ae35de1586fd7509c97c063403f8
Diego Brito <dibbrito@gmail.com>
2025-01-29 20:24:05 -0300

    simplify solution for Candy

--- src/main/java/com/leetcode/arrays/Candy.java
+++ src/main/java/com/leetcode/arrays/Candy.java
@@ -1,5 +1,7 @@
 package com.leetcode.arrays;
 
+import java.util.Arrays;
+
 public class Candy {
 
     //    https://leetcode.com/problems/candy/
@@ -8,23 +10,18 @@ public class Candy {
     public int candy(int[] ratings) {
         int size = ratings.length;
         int[] candies = new int[size];
-        for (int i = 0; i < size; i++) {
-            candies[i] = 1;
-        }
+        Arrays.fill(candies, 1);
         for (int i = 1; i < size; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
-            }
+            if (ratings[i] > ratings[i - 1]) // if the current rating is greater than the left rating, then the current candy should be greater than the left candy.
+                candies[i] = candies[i - 1] + 1; // to make sure the current candy is greater than the left candy.
         }
         for (int i = size - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
-            }
+            if (ratings[i] > ratings[i + 1]) // if the current rating is greater than the right rating, then the current candy should be greater than the right candy.
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1); // to make sure the current candy is greater than the right candy.
         }
         int sum = 0;
-        for (int candy : candies) {
+        for (int candy : candies)
             sum += candy;
-        }
         return sum;
     }
 }
