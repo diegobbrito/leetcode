dab51916d5cf92e292eb76cd2ce7addc2bd31173
Diego Brito <dibbrito@gmail.com>
2025-02-07 18:48:15 -0300

    Add binary search solution for Special Array With X Elements Greater Than or Equal X

--- src/main/java/com/leetcode/arrays/SpecialArrayWithXElementsGreaterThanEqualX.java
+++ src/main/java/com/leetcode/arrays/SpecialArrayWithXElementsGreaterThanEqualX.java
@@ -20,4 +20,25 @@ public class SpecialArrayWithXElementsGreaterThanEqualX {
         }
         return -1;
     }
+
+    //Time complexity: O(nlogn)
+    //Space complexity: O(1)
+    public int specialArrayBinarySearch(int[] nums) {
+
+        int left = 1;
+        int right = nums.length;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int count = 0;
+            for (int num : nums)
+                if (num >= mid) count++;
+            if (count == mid) return mid;
+            else if (count > mid)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1;
+    }
+
 }
