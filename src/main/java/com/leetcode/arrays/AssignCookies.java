59a36311fbc004752ee21f5ac0d71b09868ab36c
Diego Brito <54558396+Diegobbrito@users.noreply.github.com>
2025-03-03 22:56:33 +0000

    Add solution for Assign Cookies

---
+++ src/main/java/com/leetcode/arrays/AssignCookies.java
@@ -0,0 +1,23 @@
+package com.leetcode.arrays;
+
+public class AssignCookies {
+    //    https://leetcode.com/problems/assign-cookies/
+//    Time Complexity O(GlogG + SlogS + Max g|s)
+//    Memory Complexity O(g + s)
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int j = 0;
+        int i = 0;
+        int gSize = g.length;
+        int sSize = s.length;
+        while(i < gSize && j < sSize){
+            if(g[i] <= s[j]){
+                i++;
+            } 
+            j++;
+        }
+
+        return i;        
+    }
+}
