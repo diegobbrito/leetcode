fbab22e7ba24ddc509d13c40903a65aefff69dc4
Diego Brito <dibbrito@gmail.com>
2025-04-10 23:19:00 -0300

    Add solution for Hand Straights

---
+++ src/main/java/com/leetcode/greedy/HandStraights.java
@@ -0,0 +1,32 @@
+package com.leetcode.greedy;
+
+public class HandStraights {
+//    https://leetcode.com/problems/hand-of-straights/
+
+    //  Time complexity: O(n)
+    //  Space complexity: O(n)
+
+        public boolean isNStraightHand(int[] hand, int groupSize) {
+        if (hand.length % groupSize != 0) return false;
+
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : hand) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+        
+        for (int num : hand) {
+            int start = num;
+            while (count.getOrDefault(start - 1, 0) > 0) start--;
+            while (start <= num) {
+                while (count.getOrDefault(start, 0) > 0) {
+                    for (int i = start; i < start + groupSize; i++) {
+                        if (count.getOrDefault(i, 0) == 0) return false;
+                        count.put(i, count.get(i) - 1);
+                    }
+                }
+                start++;
+            }
+        }
+        return true;
+    }
+}
