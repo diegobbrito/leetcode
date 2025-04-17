e6d12e838f907f566115aa13fd5f47346f35e60b
Diegobbrito <dibbrito@gmail.com>
2025-04-17 01:29:04 +0000

    Add solution for Partition Labels

---
+++ src/main/java/com/leetcode/greedy/PartitionLabels.java
@@ -0,0 +1,29 @@
+package com.leetcode.greedy;
+
+public class PartitionLabels {
+    
+    //    https://leetcode.com/problems/partition-labels/
+    //    Time Complexity O(n)
+    //    Memory Complexity O(m)
+    
+    public List<Integer> partitionLabels(String s) {
+        Map<Character, Integer> lastIndex = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex.put(s.charAt(i), i);
+        }
+        
+        List<Integer> res = new ArrayList<>();
+        int size = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            size++;
+            end = Math.max(end, lastIndex.get(s.charAt(i)));
+
+            if (i == end) {
+                res.add(size);
+                size = 0;
+            }
+        }
+        return res;
+    }
+
+}
