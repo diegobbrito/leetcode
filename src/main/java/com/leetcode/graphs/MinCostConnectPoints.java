d52358f53ab99f4a333cf8d6e940e90a61580bab
Diego Brito <dibbrito@gmail.com>
2025-04-29 22:36:08 -0300

    update solution

--- src/main/java/com/leetcode/graphs/MinCostConnectPoints.java
+++ src/main/java/com/leetcode/graphs/MinCostConnectPoints.java
@@ -9,28 +9,27 @@ public class MinCostConnectPoints {
     // Space complexity: O(n)
 
     public int minCostConnectPoints(int[][] points) {
-        int n = points.length, node = 0;
-        int[] dist = new int[n];
-        boolean[] visit = new boolean[n];
+        int length = points.length, node = 0;
+        int[] dist = new int[length];
+        boolean[] visit = new boolean[length];
         Arrays.fill(dist, 100000000);
-        int edges = 0, res = 0;
+        int edges = 0, result = 0;
 
-        while (edges < n - 1) {
+        while (edges < length - 1) {
             visit[node] = true;
             int nextNode = -1;
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < length; i++) {
                 if (visit[i]) continue;
-                int curDist = Math.abs(points[i][0] - points[node][0]) +
-                        Math.abs(points[i][1] - points[node][1]);
+                int curDist = Math.abs(points[i][0] - points[node][0]) + Math.abs(points[i][1] - points[node][1]);
                 dist[i] = Math.min(dist[i], curDist);
                 if (nextNode == -1 || dist[i] < dist[nextNode]) {
                     nextNode = i;
                 }
             }
-            res += dist[nextNode];
+            result += dist[nextNode];
             node = nextNode;
             edges++;
         }
-        return res;
+        return result;
     }
 }
