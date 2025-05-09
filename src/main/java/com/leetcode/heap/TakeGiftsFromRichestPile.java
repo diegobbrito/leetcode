5c6138f8ee53b40a4aeef9bf41a4ee76da7554b9
Diego Brito <dibbrito@gmail.com>
2025-05-09 20:18:06 -0300

    Update solution for Take Gifts From the Richest Pile

--- src/main/java/com/leetcode/heap/TakeGiftsFromRichestPile.java
+++ src/main/java/com/leetcode/heap/TakeGiftsFromRichestPile.java
@@ -4,12 +4,16 @@ import java.util.PriorityQueue;
 
 public class TakeGiftsFromRichestPile {
 
+    // https://leetcode.com/problems/take-gifts-from-the-richest-pile/
+    // Time complexity: O(k * log n)
+    // Space complexity: O(n)
+
     public long pickGifts(int[] nums, int k) {
         int n = nums.length;
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));    // MAX-HEAP
 
-        for (int i = 0; i < n; i++) {
-            pq.add(nums[i]);
+        for (int num : nums) {
+            pq.add(num);
         }
 
         long sum = 0;
@@ -17,7 +21,7 @@ public class TakeGiftsFromRichestPile {
             pq.add((int) Math.sqrt(pq.remove()));
         }
 
-        while (pq.size() > 0) {
+        while (!pq.isEmpty()) {
             sum += pq.remove();
         }
 
