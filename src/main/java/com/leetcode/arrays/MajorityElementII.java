388933a856eac0061a1b4b3a724106b83f48e7da
Diego Brito <dibbrito@gmail.com>
2025-05-14 20:36:03 -0300

    Add a solution for Majority Element II with SC of O(1)

--- src/main/java/com/leetcode/arrays/MajorityElementII.java
+++ src/main/java/com/leetcode/arrays/MajorityElementII.java
@@ -28,6 +28,43 @@ public class MajorityElementII {
         }
 
         return result;
+    }
 
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+
+    public List<Integer> majorityElement2(int[] nums) {
+        int num1 = -1, num2 = -1, count1 = 0, count2 = 0;
+
+        for (int num : nums) {
+            if (num == num1) count1++;
+            else if (num == num2) count2++;
+            else if (count1 == 0) {
+                count1 = 1;
+                num1 = num;
+            } else if (count2 == 0) {
+                count2 = 1;
+                num2 = num;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == num1)
+                count1++;
+            else if (num == num2)
+                count2++;
+        }
+
+        int n = nums.length;
+        List<Integer> res = new ArrayList<>();
+        if (count1 > n / 3) res.add(num1);
+        if (count2 > n / 3) res.add(num2);
+
+        return res;
     }
 }
