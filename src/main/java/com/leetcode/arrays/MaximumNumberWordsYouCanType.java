f71faf259560d421338e6eaf170e23c6fe46ccf1
Diego Brito <dibbrito@gmail.com>
2025-09-15 20:00:29 -0300

    Add comments to MaximumNumberWordsYouCanType for clarity on time and space complexity

--- src/main/java/com/leetcode/arrays/MaximumNumberWordsYouCanType.java
+++ src/main/java/com/leetcode/arrays/MaximumNumberWordsYouCanType.java
@@ -5,6 +5,10 @@ import java.util.Set;
 
 public class MaximumNumberWordsYouCanType {
 
+    //    https://leetcode.com/problems/maximum-number-of-words-you-can-type/
+    //    Time complexity: O(n * m) n - number of words, m - average length of a word
+    //    Space complexity: O(k) k - number of broken letters
+
     public int canBeTypedWords(String text, String brokenLetters) {
 
         String[] words = text.split(" ");
