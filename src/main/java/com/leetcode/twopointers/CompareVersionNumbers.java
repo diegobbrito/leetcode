17a9e9dfd323bbec31d91463bbb80ea94bd0936f
Diego Brito <dibbrito@gmail.com>
2025-09-23 21:36:26 -0300

    Add implementation for CompareVersionNumbers to compare version strings

---
+++ src/main/java/com/leetcode/twopointers/CompareVersionNumbers.java
@@ -0,0 +1,24 @@
+package com.leetcode.twopointers;
+
+public class CompareVersionNumbers {
+
+
+    // https://leetcode.com/problems/compare-version-numbers/
+    // Time complexity: O(n + m)
+    // Space complexity: O(n + m)
+    public int compareVersion(String version1, String version2) {
+        final String[] levels1 = version1.split("\\.");
+        final String[] levels2 = version2.split("\\.");
+        final int length = Math.max(levels1.length, levels2.length);
+
+        for (int i = 0; i < length; ++i) {
+            final Integer v1 = i < levels1.length ? Integer.parseInt(levels1[i]) : 0;
+            final Integer v2 = i < levels2.length ? Integer.parseInt(levels2[i]) : 0;
+            final int compare = v1.compareTo(v2);
+            if (compare != 0)
+                return compare;
+        }
+
+        return 0;
+    }
+}
