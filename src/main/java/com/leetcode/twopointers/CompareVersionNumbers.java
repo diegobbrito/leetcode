85f8694de6f6aedb301de3813e68a2a10e11b9b5
Diego Brito <dibbrito@gmail.com>
2025-09-28 10:40:05 -0300

    Add two pointers implementation for version comparison

--- src/main/java/com/leetcode/twopointers/CompareVersionNumbers.java
+++ src/main/java/com/leetcode/twopointers/CompareVersionNumbers.java
@@ -21,4 +21,30 @@ public class CompareVersionNumbers {
 
         return 0;
     }
+
+    // Two pointers approach
+    public int compareVersionTwoPointers(String version1, String version2) {
+        int n1 = version1.length(), n2 = version2.length();
+        int i = 0, j = 0;
+        while (i < n1 || j < n2) {
+            int v1 = 0;
+            while (i < n1 && version1.charAt(i) != '.') {
+                v1 = v1 * 10 + (version1.charAt(i) - '0');
+                i++;
+            }
+            i++; // skip the dot
+
+            int v2 = 0;
+            while (j < n2 && version2.charAt(j) != '.') {
+                v2 = v2 * 10 + (version2.charAt(j) - '0');
+                j++;
+            }
+            j++; // skip the dot
+
+            if (v1 != v2) {
+                return Integer.compare(v1, v2);
+            }
+        }
+        return 0;
+    }
 }
