3fbb01a1fb85bea94b8148409323955db772b020
Diego Brito <dibbrito@gmail.com>
2026-08-12 20:58:19 -0300

    feat: add SQL query to calculate special bonus for employees

---
+++ src/main/java/com/leetcode/slq/CalculateSpecialBonus.sql
@@ -0,0 +1,10 @@
+-- https://neetcode.io/problems/sql-calculate-special-bonus/question
+SELECT employee_id,
+CASE
+    WHEN (employee_id % 2 = 0 OR name LIKE 'M%') THEN 0
+    ELSE salary
+END AS bonus
+
+FROM employees
+
+ORDER BY employee_id;
