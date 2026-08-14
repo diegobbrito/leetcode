539fec282ac6a3f7a07f5afa974ac8955c467cd8
Diego Brito <dibbrito@gmail.com>
2026-08-14 18:39:01 -0300

    feat: add SQL query to combine two tables for person and address

---
+++ src/main/java/com/leetcode/slq/CombineTwoTables.sql
@@ -0,0 +1,6 @@
+-- https://neetcode.io/problems/sql-combine-two-tables/question
+
+SELECT p.first_name, p.last_name, a.city, a.state
+FROM person AS p
+LEFT JOIN address AS a
+ON p.person_id = a.person_id;
