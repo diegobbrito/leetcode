57787ced7c367d20e98af9e5f945ff34ff800e7c
Diego Brito <dibbrito@gmail.com>
2026-09-04 21:26:53 -0300

    feat: add MaximumAscendingSubarraySum class with method to calculate maximum ascending subarray sum

---
+++ src/main/java/com/leetcode/arrays/MaximumAscendingSubarraySum.java
@@ -0,0 +1,22 @@
+package com.leetcode.arrays;
+
+public class MaximumAscendingSubarraySum {
+//    https://leetcode.com/problems/maximum-ascending-subarray-sum/
+//    Time complexity: O(n)
+//    Space complexity: O(1)
+
+    public int maxAscendingSum(int[] nums) {
+        int max = nums[0];
+        int curr = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] <= nums[i - 1]){
+                curr = nums[i];
+            } else {
+                curr += nums[i];
+                max = Math.max(max, curr);
+            }
+        }
+
+        return max;
+    }
+}
