31526ed9c9c74d4e3b9af3da3eb41c980b003c44
Diego Brito <dibbrito@gmail.com>
2026-09-17 19:38:39 -0300

    refactor: update comments for time and space complexity in SignProductArray class

--- src/main/java/com/leetcode/arrays/SignProductArray.java
+++ src/main/java/com/leetcode/arrays/SignProductArray.java
@@ -2,8 +2,8 @@ package com.leetcode.arrays;
 
 public class SignProductArray {
     //    https://leetcode.com/problems/sign-of-the-product-of-an-array/
-//    Time Complexity O(n)
-//    Memory Complexity O(1)
+    //    Time Complexity O(n)
+    //    Memory Complexity O(1)
     public int arraySign(int[] nums) {
         int result = 1;
         for (int num : nums) {
